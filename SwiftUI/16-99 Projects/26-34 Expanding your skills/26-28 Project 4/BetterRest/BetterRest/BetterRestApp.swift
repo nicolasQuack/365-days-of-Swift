0f26035292f8e0de997533e2a4038b80f3d67ede
nicolasQuack <nicolas.quack@icloud.com>
2023-11-21 13:10:26 -0300

    feat: adding the BetterRest project

---
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/26-28 Project 4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nicolas Florencio Felipe on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/26-28 Project 4/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Nicolas Florencio Felipe on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+}
+
+#Preview {
+    ContentView()
+}
