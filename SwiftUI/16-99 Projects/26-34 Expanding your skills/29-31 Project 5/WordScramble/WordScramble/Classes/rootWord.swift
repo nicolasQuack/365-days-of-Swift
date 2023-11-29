1466ff30f95df2fd2bb76ab508f6a2348074c414
nicolasQuack <nicolas.quack@icloud.com>
2023-11-29 11:09:17 -0300

    feat: adding rootWord class;

---
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/29-31 Project 5/WordScramble/WordScramble/Classes/rootWord.swift
@@ -0,0 +1,14 @@
+//
+//  rootWord.swift
+//  WordScramble
+//
+//  Created by Nicolas Florencio Felipe on 29/11/23.
+//
+
+import Foundation
+
+struct rootWordClass: Hashable {
+    var word: String
+    var score: Int
+    
+}
