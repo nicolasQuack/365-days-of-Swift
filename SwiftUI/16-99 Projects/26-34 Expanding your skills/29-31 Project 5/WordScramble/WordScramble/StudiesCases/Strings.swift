6df96cb55b4776bf048f034118141b53bc3a2734
nicolasQuack <nicolas.quack@icloud.com>
2023-11-28 23:27:15 -0300

    docs: comment studies documents;

--- SwiftUI/16-99 Projects/26-34 Expanding your skills/29-31 Project 5/WordScramble/WordScramble/StudiesCases/Strings.swift
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/29-31 Project 5/WordScramble/WordScramble/StudiesCases/Strings.swift
@@ -4,16 +4,16 @@
 //
 //  Created by Nicolas Florencio Felipe on 27/11/23.
 //
-
-import SwiftUI
-
-struct Strings: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-    
-    func workWithStrings() {
-        
+//
+//import SwiftUI
+//
+//struct Strings: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//    
+//    func workWithStrings() {
+//        
 //        let input = """
 //                  a
 //                  b
@@ -22,16 +22,16 @@ struct Strings: View {
 //        let letters = input.components(separatedBy: "\n")
 //        let letter = letters.randomElement()
 //        let trimming = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        let word = "Swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-    }
-    
-}
-
-#Preview {
-    Strings()
-}
+//        
+//        let word = "Swift"
+//        let checker = UITextChecker()
+//        let range = NSRange(location: 0, length: word.utf16.count)
+//        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+//        let allGood = misspelledRange.location == NSNotFound
+//    }
+//    
+//}
+//
+//#Preview {
+//    Strings()
+//}
--- SwiftUI/16-99 Projects/26-34 Expanding your skills/29-31 Project 5/WordScramble/WordScramble/StudiesCases/URLs.swift
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/29-31 Project 5/WordScramble/WordScramble/StudiesCases/URLs.swift
@@ -4,25 +4,25 @@
 //
 //  Created by Nicolas Florencio Felipe on 26/11/23.
 //
-
-import SwiftUI
-
-struct URLs: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-    
-    func testBundle () {
-        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            // we found the file in our bundle!
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
-            }
-        }
-    }
-    
-}
-
-#Preview {
-    URLs()
-}
+//
+//import SwiftUI
+//
+//struct URLs: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//    
+//    func testBundle () {
+//        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+//               we found the file in our bundle!
+//            if let fileContents = try? String(contentsOf: fileURL) {
+//               we loaded the file into a string!
+//            }
+//        }
+//    }
+//    
+//}
+//
+//#Preview {
+//    URLs()
+//}
