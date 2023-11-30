2052242dcb293d7671466405e4c28ba1c2c28f85
nicolasQuack <nicolas.quack@icloud.com>
2023-11-30 08:35:33 -0300

    feat: initializing project 6: Animations;

---
+++ SwiftUI/16-99 Projects/26-34 Expanding your skills/32-34 Project 6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Nicolas Florencio Felipe on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
