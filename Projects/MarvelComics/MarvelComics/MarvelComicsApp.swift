15a6d8a78bc657dfed4a70452e1ca351d8c16859
nicolasQuack <nicolas.quack@icloud.com>
2023-11-30 11:50:45 -0300

    initializing marvel comics personal project;

---
+++ Projects/MarvelComics/MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Nicolas Florencio Felipe on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
