eae656b5b13f7bad58cda681c049b7e3d2a87d71
nicolasQuack <nicolas.quack@icloud.com>
2023-12-04 12:30:30 -0300

    feat: creating the marvel comics project;

---
+++ Projects/MarbleComics/MarbleComics/MarbleComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarbleComicsApp.swift
+//  MarbleComics
+//
+//  Created by Nicolas Florencio Felipe on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MarbleComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
