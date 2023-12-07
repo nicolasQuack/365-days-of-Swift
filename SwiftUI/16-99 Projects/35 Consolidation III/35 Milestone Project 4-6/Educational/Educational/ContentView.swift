627a66b5028d52c897e13644facd44c1ac48f74a
nicolasQuack <nicolas.quack@icloud.com>
2023-12-07 12:23:46 -0300

    feat: finished first screen(select of difficulty and number of questions);

--- SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/Educational/Educational/ContentView.swift
+++ SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/Educational/Educational/ContentView.swift
@@ -8,40 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Easy : 2...4
+    // Moderate: 5...8
+    // Hard: 9...12
+    
     @State private var difficultyLevelLabel: Array<String> = ["Easy", "Moderate", "Hard"]
     @State private var difficultyLevel: Array<Difficulty> = [.easy, .moderate, .hard]
     @State private var rangeOfQuestions: Array<Int> = [5, 10, 15]
     
     @State private var currentDifficulty: Difficulty = .moderate
+    @State private var currentNumOfQuestions: Int = 10
     
     var body: some View {
         VStack {
-            Text("Select difficulty:")
-                .font(.largeTitle)
-                .fontWeight(.light)
-                .shadow(radius: 20)
-                .padding(.vertical)
+            
+            //Select difficulty
+            VStack {
+                Text("Select difficulty:")
+                    .textStyle()
 
-            HStack (spacing: 20) {
-                ForEach(0..<difficultyLevelLabel.count, id: \.self) { num in
-                    Button () {
-                        currentDifficulty = difficultyLevel[num]
-                        print(currentDifficulty)
-                    } label: {
-                        Rectangle()
-                            .fill(num == 0 ? .green : num == 1 ? .yellow : .red)
-                            .clipShape(.buttonBorder)
-                            .font(.subheadline)
-                            .foregroundStyle(.primary)
-                            .shadow(radius: 10)
+                HStack (spacing: 20) {
+                    ForEach(0..<difficultyLevelLabel.count, id: \.self) { num in
+                        Button () {
+                            currentDifficulty = difficultyLevel[num]
+                        } label: {
+                            Rectangle()
+                                .fill(num == 0 ? .green : num == 1 ? .yellow : .red)
+                                .clipShape(.buttonBorder)
+                                .font(.subheadline)
+                                .foregroundStyle(.primary)
+                                .shadow(radius: 10)
+                                .frame(width: 100, height: 100)
+                        }
                     }
-                    .frame(width: 100, height: 100)
-                    
                 }
             }
+            .padding(.top, 100)
             
-            
+            //Number of Questions
+            VStack {
+                Text("Number of Questions:")
+                    .textStyle()
 
+                Picker("hello", selection: $currentNumOfQuestions) {
+                    ForEach (rangeOfQuestions, id: \.self) {
+                        Text(String($0))
+                    }
+                }
+                .pickerStyle(.palette)
+                .shadow(radius: 10)
+                .frame(width: 340)
+            }
+            .padding(.top, 100)
+            
+            //Generate button
+            VStack {
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .fill(.thinMaterial)
+                            .frame(width: 100, height: 50)
+                            .clipShape(.buttonBorder)
+                            .shadow(radius: 10)
+                        Text("Generate")
+                            .foregroundStyle(.black)
+                            
+                            
+                        
+                    }
+                }
+            }
+            .padding(.top, 100)
+            
+            Spacer()
             
         }
     }
