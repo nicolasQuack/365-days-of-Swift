84125f9c21403391976fb17d506bfd38dffb1039
nicolasQuack <nicolas.quack@icloud.com>
2023-12-07 11:22:40 -0300

    feat: adding the select difficulty options;

---
+++ SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/Educational/Educational/Classes/Difficulty.swift
@@ -0,0 +1,12 @@
+//
+//  Difficulty.swift
+//  Educational
+//
+//  Created by Nicolas Florencio Felipe on 07/12/23.
+//
+
+import Foundation
+
+enum Difficulty {
+    case easy, moderate, hard
+}
--- SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/Educational/Educational/ContentView.swift
+++ SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/Educational/Educational/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var difficultyLevelLabel: Array<String> = ["Easy", "Moderate", "Hard"]
+    @State private var difficultyLevel: Array<Difficulty> = [.easy, .moderate, .hard]
+    @State private var rangeOfQuestions: Array<Int> = [5, 10, 15]
+    
+    @State private var currentDifficulty: Difficulty = .moderate
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Select difficulty:")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .shadow(radius: 20)
+                .padding(.vertical)
+
+            HStack (spacing: 20) {
+                ForEach(0..<difficultyLevelLabel.count, id: \.self) { num in
+                    Button () {
+                        currentDifficulty = difficultyLevel[num]
+                        print(currentDifficulty)
+                    } label: {
+                        Rectangle()
+                            .fill(num == 0 ? .green : num == 1 ? .yellow : .red)
+                            .clipShape(.buttonBorder)
+                            .font(.subheadline)
+                            .foregroundStyle(.primary)
+                            .shadow(radius: 10)
+                    }
+                    .frame(width: 100, height: 100)
+                    
+                }
+            }
+            
+            
+
+            
         }
-        .padding()
     }
 }
 
