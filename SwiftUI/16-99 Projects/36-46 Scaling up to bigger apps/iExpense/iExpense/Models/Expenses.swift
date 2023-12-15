08881888d040fcb5027b74eb51dffb65214bedeb
nicolasQuack <nicolas.quack@icloud.com>
2023-12-15 11:35:34 -0300

    feat: adding list of items;

---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/Models/Expenses.swift
@@ -0,0 +1,14 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 15/12/23.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
