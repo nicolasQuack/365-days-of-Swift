1e7727ad3d38054fc137d4ef1d70e1ed478b9c01
nicolasQuack <nicolas.quack@icloud.com>
2023-12-15 10:41:13 -0300

    feat: adding a group of models for structs, classes, enums and so on;

---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/Models/ExepenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExepenseItem.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 15/12/23.
+//
+
+import Foundation
---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/Models/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 15/12/23.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
