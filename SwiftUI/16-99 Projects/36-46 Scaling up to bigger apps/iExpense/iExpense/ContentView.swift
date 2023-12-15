70138075374a9352d49c9dbb2d254f7ecb998d67
nicolasQuack <nicolas.quack@icloud.com>
2023-12-15 01:07:20 -0300

    feat: creating iExpensive project;

---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CodableView()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
