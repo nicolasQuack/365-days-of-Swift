cfefdffd35ab3aa0b9780241a5e42044d7187c86
nicolasQuack <nicolas.quack@icloud.com>
2023-12-15 11:35:54 -0300

    feat: creating the view of user;

--- SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/ContentView.swift
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/ContentView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense  ")
+            .toolbar {
+                Button ("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
+    }
+    
+    func removeItems(at offSet: IndexSet) {
+        expenses.items.remove(atOffsets: offSet)
     }
 }
 
