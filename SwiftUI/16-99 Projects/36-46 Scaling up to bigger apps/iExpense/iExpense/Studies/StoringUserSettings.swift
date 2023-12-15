c779ac8e69028e641b0dd3c3be1a34a6e38c8d71
nicolasQuack <nicolas.quack@icloud.com>
2023-12-15 01:08:41 -0300

    feat: learning about store user settings;

---
+++ SwiftUI/16-99 Projects/36-46 Scaling up to bigger apps/iExpense/iExpense/Studies/StoringUserSettings.swift
@@ -0,0 +1,23 @@
+//
+//  StoringUserSettings.swift
+//  iExpense
+//
+//  Created by Nicolas Florencio Felipe on 14/12/23.
+//
+
+import SwiftUI
+
+struct StoringUserSettings: View {
+    @AppStorage("tapCount") private var tapCount = 0
+//    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+//            UserDefaults.standard.set(tapCount, forKey: "Tap")
+        }
+    }
+}
+
+#Preview {
+    StoringUserSettings()
+}
