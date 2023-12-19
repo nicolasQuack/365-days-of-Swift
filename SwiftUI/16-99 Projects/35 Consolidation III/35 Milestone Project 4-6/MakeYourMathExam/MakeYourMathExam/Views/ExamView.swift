3e05288e10d615a72fb8541775010bb496526db7
nicolasQuack <nicolas.quack@icloud.com>
2023-12-19 17:55:09 -0300

    feat: adding the exam view with navigation to view before and after;

--- SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/MakeYourMathExam/MakeYourMathExam/Views/ExamView.swift
+++ SwiftUI/16-99 Projects/35 Consolidation III/35 Milestone Project 4-6/MakeYourMathExam/MakeYourMathExam/Views/ExamView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ExamView: View {
     
+    @State var exam: Exam
+    
     var body: some View {
-        Button ("Test") {
-
+        NavigationStack {
+            List {
+                ForEach(exam.questions) { question in
+                    NavigationLink ("\(question.x) X \(question.y) = \(question.userAnswer)") {
+                        QuestionsModel(question: question)
+                    }
+                }
+            }
+            .navigationTitle("Exam: \(navTitleValidation())/\(exam.questions.count)")
+            .navigationBarTitleDisplayMode(.inline)
+          
+            
+            NavigationLink ("Hand Over") {
+                TestResult(exam: exam)
+            }
+            .disabled(buttonValidation())
+            .padding()
+        }
+        
+    }
+    
+    func buttonValidation () -> Bool {
+        var isResponse = [Bool]()
+        
+        for i in exam.questions {
+            if i.userAnswer != "" {
+                isResponse.append(true)
+            } else {
+                isResponse.append(false)
+            }
+        }
+        if isResponse.contains(false) {
+            return true
+        } else {
+            return false
         }
     }
+    
+    func navTitleValidation () -> Int {
+        var count = 0
+        
+        for j in exam.questions {
+            if j.userAnswer != "" {
+                count += 1
+            }
+        }
+        return count
+    }
+    
 }
 
 #Preview {
-    ExamView()
+    ExamView(exam: Exam())
 }
